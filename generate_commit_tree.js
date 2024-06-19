853450b53615a98ef0b8ecd8b30712651de2b3dd
sanjeev kumar <sksanjeev362@gmail.com>
2024-06-19 10:50:20 +0530

    Added Commit Treee

--- generate_commit_tree.js
+++ generate_commit_tree.js
@@ -1,6 +1,6 @@
 const { execSync } = require('child_process');
 const { writeFile } = require('fs-extra');
-const { Gitgraph } = require('@gitgraph/js');
+const { Gitgraph } = require('@gitgraph/node');
 const { createCanvas } = require('canvas');
 
 // Function to generate the git graph
@@ -15,20 +15,20 @@ const generateGitGraph = async () => {
   const gitgraph = new Gitgraph(ctx);
 
   // Get commits from git log
-  const log = execSync('git log --pretty=format:"%h %p %s" --graph --all').toString();
+  const log = execSync('git log --pretty=format:"%h %p %s"').toString();
   const commits = log.split('\n').map(line => {
     const [hash, parent, ...message] = line.trim().split(' ');
     return { hash, parent, message: message.join(' ') };
   });
 
   const branches = new Map();
-  const main = gitgraph.branch('master');
-  branches.set('master', main);
+  const master = gitgraph.branch('master');
+  branches.set('master', master);
 
   // Process commits
   commits.forEach(commit => {
     const { hash, parent, message } = commit;
-    const parentBranch = branches.get(parent) || main;
+    const parentBranch = branches.get(parent) || master;
     const newBranch = parentBranch.branch(hash);
     newBranch.commit(message);
     branches.set(hash, newBranch);
