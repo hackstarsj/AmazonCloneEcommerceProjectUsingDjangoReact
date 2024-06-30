3f0db02cd053e5d7a5ffa45c894e4dee44b02826
sanjeev kumar <sksanjeev362@gmail.com>
2024-06-30 22:46:59 +0530

    Deploy CI CD on Test Server

--- Backend/EcommerceInventory/EcommerceInventory/settings.py
+++ Backend/EcommerceInventory/EcommerceInventory/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG','True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Directory for collectstatic
 
